0b3ffc67f3acf628158d110a35d4f0950aa41859
Guanti Giuseppe <gguanti@gmail.com>
2026-01-13 09:41:32 +0100

    Aggiornamento composer.lock compatibile con Laravel Cloud

--- resources/js/Pages/Appointments/Calendar.vue
+++ resources/js/Pages/Appointments/Calendar.vue
@@ -690,7 +690,7 @@ const getFilteredEvents = () =>
         end: appointment.DataFine,
         backgroundColor:
             appointment.status === "Da Pianificare"
-                ? "#FEF3C7"
+                ? "#FAFAFA"
                 : appointment.status === "Pianificato"
                 ? "#F59E0B"
                 : appointment.status === "Completato"
